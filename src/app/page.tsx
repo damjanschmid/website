526171014d7d305e74618dc5f244aae969b071b1
Damjan Schmid <schmidd2@bzz.ch>
2026-02-03 13:57:43 +0100

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-white">
+      <main className="text-3xl font-mono text-black">
+        Hello world
+      </main>
+    </div>
+  );
+}
