16ecfbec42c02669fedb155c3513ed91400cc676
damjanschmid <damjan.schmid@gmail.com>
2026-02-04 09:45:54 +0100

    text black

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-white">
-      <main className="text-3xl">
+      <main className="text-3xl text-black">
         Hello world
       </main>
     </div>
